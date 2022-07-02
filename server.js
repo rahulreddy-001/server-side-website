eb69f6cc78f2b063b84820778678aa0f53e24659
Rahul Reddy <rahulreddya5525@gmail.com>
2022-07-02 22:28:09 +0530

    added server-side-app

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const Datastore = require("nedb");
+const app = express();
+app.use(express.static("Public"));
+app.use(express.json({ limit: "10mb" }));
+
+const database = new Datastore("database.db");
+database.loadDatabase();
+
+app.get("/api", (req, res) => {
+  database.find({}, (err, data) => {
+    if (err) res.send(err);
+    res.json(data);
+  });
+});
+app.post("/api", (req, res) => {
+  let data = req.body;
+  data.timestamp = new Date();
+  database.insert(data);
+  res.json({
+    status: "success",
+  });
+});
+
+app.listen(3500, () => console.log("Listening at port:3500"));
